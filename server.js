713af624b50641a03c6bf625100a338c5db6acef
Eduardo Patrocinio <epatro@gmail.com>
2019-10-25 14:14:54 -0400

    Enahnce app

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
+const URL = 'https://rates-perf-006.cloudant.com';
+
 let portNumber = process.env.PORT_NUMBER;
 const express = require("express");
 
 const http = require("request");
 const app = express();
 
-
 const handleWelcomeRequest = async (req, res, next) => {
-  console.log ("Sending welcome request...");
-  http.get({
-    url: 'https://rates-perf-006.cloudant.com',
-    agent: false  // Create a new agent just for this one request
-  }, function(res) {
-    console.log ("Result: " + res)
-  });
-  res.status(200).send("success");
-  return;
+  console.log (Date.now() + " Sending welcome request...");
+  http (URL, (err, response, body) => {
+    if (err) {
+      console.log (err);
+      res.status(500).send(err);
+    }
+    console.log(body);
+    res.status(200).send(body);
+    return;
+  })
 }
 
 app.get("/mini/api/welcome", handleWelcomeRequest);
